d82ec0d0cdffbb9794dac7866c2954c1ab411242
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-19 01:29:30 +0700

    style: remove active background on navbar menu links

--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -1,37 +1,37 @@
-<div class="top-infobar d-none d-md-block py-2">
+<div class="top-infobar d-none d-md-block" role="complementary" aria-label="Informasi Kontak">
     <div class="container-xl">
-        <div class="d-flex justify-content-between align-items-center my-1">
-            <div class="d-flex align-items-center">
-                <span class="text-white-50 small fw-medium">Pendidikan Profesi Akuntansi &bull; Fakultas Ekonomika dan Bisnis &bull; Universitas Negeri Surabaya</span>
-            </div>
-            <div class="d-flex align-items-center gap-3">
-                <a href="mailto:ppak.feb@unesa.ac.id" class="d-none d-xl-inline-flex">
-                    <i class="fa-regular fa-envelope"></i>
+        <div class="d-flex justify-content-between align-items-center">
+            <p class="top-infobar-brand m-0">
+                Pendidikan Profesi Akuntansi <span class="top-infobar-sep">&bull;</span> Fakultas Ekonomika dan Bisnis <span class="top-infobar-sep">&bull;</span> Universitas Negeri Surabaya
+            </p>
+            <div class="d-flex align-items-center" role="list">
+                <a href="mailto:ppak.feb@unesa.ac.id" class="top-infobar-link d-none d-xl-inline-flex" role="listitem">
+                    <i class="fa-regular fa-envelope" aria-hidden="true"></i>
                     <span>ppak.feb@unesa.ac.id</span>
                 </a>
-                <span class="divider d-none d-xl-inline-block"></span>
-                <a href="tel:+62318280009" class="d-none d-xl-inline-flex">
-                    <i class="fa-solid fa-phone"></i>
+                <span class="top-infobar-divider d-none d-xl-inline-block" aria-hidden="true"></span>
+                <a href="tel:+62318280009" class="top-infobar-link d-none d-xl-inline-flex" role="listitem">
+                    <i class="fa-solid fa-phone" aria-hidden="true"></i>
                     <span>+62 31 828 0009</span>
                 </a>
-                <span class="divider d-none d-xl-inline-block"></span>
-                <a href="{{ route('akademik.kalender') }}">
-                    <i class="fa-regular fa-calendar-days"></i>
+                <span class="top-infobar-divider d-none d-xl-inline-block" aria-hidden="true"></span>
+                <a href="{{ route('akademik.kalender') }}" class="top-infobar-link" role="listitem">
+                    <i class="fa-regular fa-calendar-days" aria-hidden="true"></i>
                     <span>Kalender</span>
                 </a>
-                <span class="divider"></span>
-                <a href="{{ route('kontak.unduhan') }}">
-                    <i class="fa-solid fa-download"></i>
+                <span class="top-infobar-divider" aria-hidden="true"></span>
+                <a href="{{ route('kontak.unduhan') }}" class="top-infobar-link" role="listitem">
+                    <i class="fa-solid fa-download" aria-hidden="true"></i>
                     <span>Unduhan</span>
                 </a>
-                <span class="divider"></span>
-                <a href="{{ route('admisi.faq') }}">
-                    <i class="fa-regular fa-circle-question"></i>
+                <span class="top-infobar-divider" aria-hidden="true"></span>
+                <a href="{{ route('admisi.faq') }}" class="top-infobar-link" role="listitem">
+                    <i class="fa-regular fa-circle-question" aria-hidden="true"></i>
                     <span>FAQ</span>
                 </a>
-                <span class="divider"></span>
-                <a href="https://unesa.ac.id" target="_blank" rel="noopener noreferrer">
-                    <i class="fa-solid fa-building-columns"></i>
+                <span class="top-infobar-divider" aria-hidden="true"></span>
+                <a href="https://unesa.ac.id" target="_blank" rel="noopener noreferrer" class="top-infobar-link" role="listitem">
+                    <i class="fa-solid fa-building-columns" aria-hidden="true"></i>
                     <span>UNESA Pusat</span>
                 </a>
             </div>
