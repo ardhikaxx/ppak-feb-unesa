0ff40f7b5f0760362e044b57fa606a23ea3ab8aa
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-19 13:13:35 +0700

    feat: model dokumentasi galeri kegiatan

---
+++ app/Models/Gallery.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Gallery extends Model
+{
+    use SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'event_date' => 'date',
+        'verified_at' => 'datetime',
+    ];
+}
