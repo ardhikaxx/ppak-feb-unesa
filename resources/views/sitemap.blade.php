45de34fe5bc8fc10420fe6e0818504c6dedeea73
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-21 04:20:18 +0700

    feat: penyempurnaan template XML sitemap dinamis

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -2,10 +2,10 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach($urls as $url)
     <url>
-        <loc>{{ $url }}</loc>
-        <lastmod>{{ now()->toAtomString() }}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.7</priority>
+        <loc>{{ $url['loc'] }}</loc>
+        @if(!empty($url['lastmod']))<lastmod>{{ $url['lastmod'] }}</lastmod>@endif
+        <changefreq>{{ $url['changefreq'] ?? 'weekly' }}</changefreq>
+        <priority>{{ $url['priority'] ?? '0.7' }}</priority>
     </url>
     @endforeach
 </urlset>
