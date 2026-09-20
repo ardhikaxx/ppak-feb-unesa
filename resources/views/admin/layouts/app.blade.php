fb91cbd6bccdf8ef0d3679cae0f57c73c9c6b597
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-20 19:30:15 +0700

    feat: penambahan menu blok konten pada sidebar CMS

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -143,6 +143,9 @@
             </a>
 
             <div class="sidebar-group">Website</div>
+            <a href="{{ route('admin.content-blocks.index') }}" class="sidebar-link {{ request()->routeIs('admin.content-blocks.*') ? 'active' : '' }}">
+                <i class="fa-solid fa-cubes"></i> Blok Konten
+            </a>
             <a href="{{ route('admin.site-settings.index') }}" class="sidebar-link {{ request()->routeIs('admin.site-settings.*') ? 'active' : '' }}">
                 <i class="fa-solid fa-gear"></i> Pengaturan Website
             </a>
