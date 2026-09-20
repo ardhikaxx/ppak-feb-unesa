42ba76772ede93c740bb51093cb8190c62024cb2
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-20 17:20:56 +0700

    feat: penambahan model AuditLog

---
+++ app/Models/AuditLog.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AuditLog extends Model
+{
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'old_values' => 'array',
+        'new_values' => 'array',
+    ];
+
+    public function auditable()
+    {
+        return $this->morphTo();
+    }
+}
