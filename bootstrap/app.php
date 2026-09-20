3ebec4fad18c0a9ada44280986e54573a9ce29c6
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-20 17:20:57 +0700

    feat: pendaftaran alias middleware admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'admin.auth' => \App\Http\Middleware\AuthenticateAdmin::class,
+            'admin.active' => \App\Http\Middleware\EnsureAdminActive::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
