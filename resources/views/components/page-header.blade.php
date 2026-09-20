835a12e388772b0a95da2c059cf2a64ce85c306b
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-21 04:20:05 +0700

    refactor: pembaruan komponen header halaman dengan breadcrumb SEO

--- resources/views/components/page-header.blade.php
+++ resources/views/components/page-header.blade.php
@@ -1,4 +1,10 @@
 @props(['title' => '', 'badge' => null, 'lead' => null, 'breadcrumbs' => []])
+{{-- BreadcrumbList JSON-LD otomatis dari breadcrumb visual yang sama --}}
+@if(!empty($breadcrumbs))
+@push('jsonld')
+<script type="application/ld+json">{!! json_encode(\App\Services\SeoService::breadcrumbJsonLd(array_merge([['label' => 'Beranda', 'url' => route('home')]], array_filter($breadcrumbs, fn ($b) => !empty($b['label'])))), JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE) !!}</script>
+@endpush
+@endif
 <section class="page-header-ppak">
     <div class="container">
         @if(!empty($breadcrumbs))
