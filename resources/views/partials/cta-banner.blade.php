d1aa69125a7e09b6110837047609f035f6300e3a
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-20 20:33:44 +0700

    feat: pengambilan CTA banner dari database

--- resources/views/partials/cta-banner.blade.php
+++ resources/views/partials/cta-banner.blade.php
@@ -6,21 +6,21 @@
                     <span class="badge-ppak badge-ppak-gold mb-3">
                         <i class="fa-solid fa-graduation-cap"></i> Penerimaan Mahasiswa Baru
                     </span>
-                    <h2 class="text-white">Bangun Kompetensi Profesional Anda di PPAk FEB UNESA</h2>
+                    <h2 class="text-white">{{ $pgGlobal['cta_heading']['heading'] ?? 'Bangun Kompetensi Profesional Anda di PPAk FEB UNESA' }}</h2>
                     <div class="golden-line"></div>
                     <p class="mb-lg-0 text-white-50">
-                        Tingkatkan kualifikasi keprofesian akuntansi Anda dengan bimbingan akademisi dan praktisi terkemuka. Tempuh jalur terpadu meraih gelar Akuntan (Ak.), pembebasan ujian sertifikasi Chartered Accountant (CA), dan keunggulan karier global.
+                        {{ $pgGlobal['cta_body']['body'] ?? 'Tingkatkan kualifikasi keprofesian akuntansi Anda dengan bimbingan akademisi dan praktisi terkemuka. Tempuh jalur terpadu meraih gelar Akuntan (Ak.), pembebasan ujian sertifikasi Chartered Accountant (CA), dan keunggulan karier global.' }}
                     </p>
                 </div>
                 <div class="col-lg-4 text-lg-end mt-4 mt-lg-0">
                     <div class="d-flex flex-column flex-sm-row flex-lg-column gap-3 justify-content-lg-end">
                         <a href="{{ route('admisi.jalur-syarat') }}" class="btn-ppak-gold btn-ppak-lg">
-                            <span>Informasi Pendaftaran</span>
+                            <span>{{ $pgGlobal['cta_btn1']['heading'] ?? 'Informasi Pendaftaran' }}</span>
                             <i class="fa-solid fa-arrow-right"></i>
                         </a>
                         <a href="{{ route('kontak.helpdesk') }}" class="btn-ppak-secondary btn-ppak-lg">
                             <i class="fa-solid fa-headset me-1"></i>
-                            <span>Hubungi Sekretariat</span>
+                            <span>{{ $pgGlobal['cta_btn2']['heading'] ?? 'Hubungi Sekretariat' }}</span>
                         </a>
                     </div>
                 </div>
