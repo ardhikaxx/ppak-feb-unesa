8687c412828934e76e211db871bc1d0ea0e8b7e3
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-21 17:18:04 +0700

    feat: integrasi SweetAlert2 ke layout publik

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,6 +78,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
 
+    @include('components.swal')
     @stack('scripts')
 </body>
 </html>
