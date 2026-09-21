e2b10ecc5ee320aff3646dd792102df3c7a6dcd1
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-21 07:43:45 +0700

    refactor: gunakan konfigurasi untuk status agenda pada index

--- resources/views/admin/agendas/index.blade.php
+++ resources/views/admin/agendas/index.blade.php
@@ -20,7 +20,7 @@
                 <div class="col-md-3">
                     <select name="status" class="form-select form-select-sm">
                         <option value="">Semua status</option>
-                        @foreach(['upcoming' => 'Mendatang', 'ongoing' => 'Berlangsung', 'completed' => 'Selesai', 'cancelled' => 'Batal'] as $val => $label)
+                        @foreach(config('ppak.options.agenda_status') as $val => $label)
                             <option value="{{ $val }}" @selected(request('status') === $val)>{{ $label }}</option>
                         @endforeach
                     </select>
