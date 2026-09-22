be26c303d2d1c015d8b6adc0566592a63cee7a8a
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-22 09:10:24 +0700

    fix: perbaiki render XML sitemap gunakan unescaped output

--- resources/views/sitemap-news.blade.php
+++ resources/views/sitemap-news.blade.php
@@ -1,7 +1,7 @@
-{{ '<?xml version="1.0" encoding="UTF-8"?>' }}
+{!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
-    @foreach($urls as $url)
+@foreach($urls as $url)
     <url>
         <loc>{{ $url['loc'] }}</loc>
         @if(!empty($url['lastmod']))<lastmod>{{ $url['lastmod'] }}</lastmod>@endif
@@ -14,5 +14,5 @@
             <news:title>{{ $url['news_title'] }}</news:title>
         </news:news>
     </url>
-    @endforeach
+@endforeach
 </urlset>
