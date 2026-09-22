e01ccc2bdf976b084152593c319ae69de9a4811e
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-22 20:23:42 +0700

    feat: pendaftaran middleware peran dan akses admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin.auth' => \App\Http\Middleware\AuthenticateAdmin::class,
             'admin.active' => \App\Http\Middleware\EnsureAdminActive::class,
+            'admin.role' => \App\Http\Middleware\EnsureAdminRole::class,
+            'admin.access' => \App\Http\Middleware\EnsureAdminRouteAccess::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
