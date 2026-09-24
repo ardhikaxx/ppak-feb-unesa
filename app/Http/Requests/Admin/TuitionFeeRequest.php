98a0f17a765a19669033ff7c528c9bc763b118cf
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-25 06:51:11 +0700

    fix: hapus aturan validasi currency yang tidak digunakan pada form biaya kuliah

--- app/Http/Requests/Admin/TuitionFeeRequest.php
+++ app/Http/Requests/Admin/TuitionFeeRequest.php
@@ -17,7 +17,6 @@ class TuitionFeeRequest extends FormRequest
             'program_name' => ['required', 'string', 'max:255'],
             'fee_type' => ['required', 'string', 'max:50'],
             'amount' => ['required', 'integer', 'min:0', 'max:1000000000'],
-            'currency' => ['nullable', 'string', 'max:10'],
             'period' => ['nullable', 'string', 'max:30'],
             'academic_year' => ['required', 'string', 'max:20', 'regex:/^\d{4}\/\d{4}$/'],
             'description' => ['nullable', 'string'],
